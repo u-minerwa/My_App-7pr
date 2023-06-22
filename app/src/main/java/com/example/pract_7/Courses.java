158aa812ed07a5266cdcfc05ff9b7d7e568ce803
Alexander <Sandr1109@yandex.ru>
2023-06-22 19:38:07 +0400

    4th Commit

---
+++ app/src/main/java/com/example/pract_7/Courses.java
@@ -0,0 +1,15 @@
+package com.example.pract_7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Courses extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_courses);
+    }
+}
+
