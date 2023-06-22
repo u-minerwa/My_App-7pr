5ec3b1fe9ec73fa8bdd07cdfdc1b97c4d7949b6e
Alexander <Sandr1109@yandex.ru>
2023-06-22 22:36:41 +0400

    7th Commit

--- app/src/main/java/com/example/pract_7/Courses.java
+++ app/src/main/java/com/example/pract_7/Courses.java
@@ -1,15 +1,43 @@
 package com.example.pract_7;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.pract_7.adapter.CategoryAdapter;
+import com.example.pract_7.model.Category;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Courses extends AppCompatActivity {
+
+    RecyclerView categoryRecycler;
+    CategoryAdapter categoryAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courses);
+
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(new Category(1, "Курсы"));
+        categoryList.add(new Category(2, "Сайты"));
+        categoryList.add(new Category(3, "Языки"));
+        categoryList.add(new Category(4, "Прочее"));
+
+        setCategoryRecycler(categoryList);
+    }
+
+    private void setCategoryRecycler(List<Category> categoryList) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+        categoryRecycler = findViewById(R.id.categoryRecycler);
+        categoryRecycler.setLayoutManager(layoutManager);
+
+        categoryAdapter = new CategoryAdapter(this, categoryList);
+        categoryRecycler.setAdapter(categoryAdapter);
     }
 }
 
